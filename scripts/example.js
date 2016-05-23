f5967088a7177e99820fbd30315fc1bb305e2a7c
nike4859 <nike4859@gmail.com>
2016-05-23 23:25:20 +0800

    tutorial 13

--- scripts/example.js
+++ scripts/example.js
@@ -1,9 +1,26 @@
 var CommentBox = React.createClass({
+	getInitialState: function(){
+		return {data:[]};
+	},
+	componentDidMount: function(){
+		$.ajax({
+			url:this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({data:data});
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+
+		});
+	},
 	render: function(){
 		return(
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
+				<CommentList data={this.state.data} />
 				<CommentForm />
 			</div>
 			);
@@ -60,7 +77,7 @@ var data = [
 ];
 
 ReactDOM.render(
-	<CommentBox data={data}/>,
+	<CommentBox url="http://localhost:3000/api/comments" />,
 	document.getElementById('content')
 );
 
