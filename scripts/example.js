e46836da39e8d64322e123afddef0ec030a7ee6e
nike4859 <nike4859@gmail.com>
2016-05-28 22:00:23 +0800

    tutorial 20

--- scripts/example.js
+++ scripts/example.js
@@ -16,16 +16,32 @@ var CommentBox = React.createClass({
 
 		});
 	},
+	handleCommentSubmit: function(comment){
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			type: 'POST',
+			data: comment,
+			success: function(data) {
+				this.setState({data:data});
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	//componentDidMount is a method called automatically by React after a component is rendered for the first time. 
 	componentDidMount: function(){
 		this.loadCommentsFromServer();
 		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
 	},
+	//把handleCommentSubmit的Function當成props(onCommentSubmit)傳
 	render: function(){
 		return(
 			<div className="commentBox">
 				<h1>Comments</h1>
 				<CommentList data={this.state.data} />
-				<CommentForm />
+				<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 			</div>
 			);
 	}
@@ -49,12 +65,37 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+	getInitialState: function(){
+		return {author: '', text: ''};
+	},
+	handleAuthorChange: function(e){
+		this.setState({author: e.target.value});
+	},
+	handleTextChange: function(e){
+		this.setState({text: e.target.value});
+	},
+	handleSubmit: function(e){
+		e.preventDefault();//阻止送出表單
+		var author = this.state.author.trim();
+		var text = this.state.text.trim();
+		if(!text || !author){//empty
+			return;
+		}
+		this.props.onCommentSubmit({author: author, text: text});
+		this.setState({author: '' ,text: ''});//clear form
+	},
 	render: function(){
 		return(
-			<div className="commentForm">
+			<div className="commentForm" onSubmit={this.handleSubmit}>
 				<form className="commentForm">
-					<input type="text" placeholder="Your name" />
-					<input type="text" placeholder="Say something..." />
+					<input type="text" placeholder="Your name"
+						value={this.state.author}
+						onChange={this.handleAuthorChange}
+					 />
+					<input type="text" placeholder="Say something..." 
+						value={this.state.text}
+						onChange={this.handleTextChange}
+					/>
 					<input type="submit" value="Post" />
 				</form>
 			</div>
